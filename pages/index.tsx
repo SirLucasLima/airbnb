aa744619fd9b2cadc840fda1130411e527bccc53
SirLucasLima <doislcompany@gmail.com>
2023-01-09 23:59:00 -0300

    mediumCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from "../components/Header"
 import Banner from "../components/Banner"
-import Static from "../data/Static.json"
+import SmallCard from "../components/SmallCard"
+import MediumCard from "../components/MediumCard"
+
+import ExploreData from "../data/ExploreData.json"
+import CardsData from "../data/CardsData.json"
 
 const Home: NextPage = () => {
   return (
@@ -19,9 +23,29 @@ const Home: NextPage = () => {
         <section className='pt-6'>
           <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
-          {Static.map((item) => (
-            <h1>{item.location}</h1>
-          ))}
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {ExploreData?.map((item, index) => (
+              <SmallCard 
+                key={index}
+                img={item.img} 
+                location={item.location} 
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {CardsData?.map((item, index) => (
+              <MediumCard 
+                key={index}
+                img={item.img}
+                title={item.title} 
+              />
+            ))}
+          </div>
         </section>
       </main>
     </div>
