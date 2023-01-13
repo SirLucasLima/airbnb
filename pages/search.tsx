fe779644c11f10f37507c434ebead675fac54130
SirLucasLima <doislcompany@gmail.com>
2023-01-13 19:12:51 -0300

    dataTags done

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,19 +1,27 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Tags from "../data/Tags.json"
+import CardsData from "../data/CardsData.json"
 
 
 export default function Search() {
   return(
     <div>
       <Header />
-
       <main className="flex">
-        <section>
+        <section className="flex-grow pt-14 px-6">
           <p className="text-xs">300+ Stays for 5 number of guests</p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
 
-          <div className="">
-            <p className="px-4 py-2 rounded-full border cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100 ease-out">Cancellation Flexibility</p>
+          <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+            {Tags?.map((item, index) => (
+              <p
+                className="button" 
+                key={index}
+              > 
+                {item.title}
+              </p>
+            ))}
           </div>
         </section>
 
