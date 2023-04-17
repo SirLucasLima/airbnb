cb45b3acc867b60a1c21ea372b12cd18d408feee
Lucas Lima <developerlucaslima@gmail.com>
2023-04-17 11:02:22 -0300

    restart project

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    images: {
-        domains: ["links.papareact.com"]
-    },
-    basePath: "",
-    env: {
-        mapbox_access_key: 'pk.eyJ1Ijoic2lybHVjYXNsaW1hIiwiYSI6ImNsY3dmaDVrbzF0NjMzcGs2NzFmZ2M1Y2gifQ.RGRmfuUp-c2ZZBIxTH7G5g',
-    }
-}
