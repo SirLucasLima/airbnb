77098bf6847a1e80923094438240abba3ae6029f
Lucas Lima <developerlucaslima@gmail.com>
2023-04-20 07:33:10 -0300

    back to old code

---
+++ data/TagsData.js
@@ -0,0 +1,17 @@
+export const TagsData = [
+  {
+    "title": "Cancellation Flexibility"
+  },
+  {
+    "title": "Type of Place"
+  },
+  {
+    "title": "Price"
+  },
+  {
+    "title": "Rooms and Beds"
+  },
+  {
+    "title": "More Filters"
+  }
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwind-scrollbar-hide')
+  ],
 }
